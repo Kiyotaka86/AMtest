3d39a086c2cc90c731623afff29e7b1367e89eb5
Kiyotaka Kokubun <kioooo.fm@gmail.com>
2016-11-11 02:00:06 -0800

    second one

--- calc.py
+++ calc.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
 
-import cgi
+import cgi, cgitb
+cgitb.enable()
 
 form = cgi.FieldStorage()
 
+shiki = form["foo".value
+#shiki = eval(form.getvalue('foo'))
 
+#result = eval(shiki)
 
 print "Content-type: text/html\n"
-print form.getvalue('foo')
+print shiki
 
 html_body="""
 <html><body>
