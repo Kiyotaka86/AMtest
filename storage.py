924b5f1bc9e87b317906f120c3ef4a219099d34e
Kiyotaka Kokubun <kioooo.fm@gmail.com>
2016-11-12 22:01:39 -0800

    deteleall and error

--- storage.py
+++ storage.py
@@ -1,27 +1,51 @@
 #!/usr/bin/env python
 
-
-import cgi, cgitb
+import cgi, cgitb, MySQLdb
 cgitb.enable()
+
+connector = MySQLdb.connect(host="localhost", db="stocker", user="kio", passwd="kio")
+cur=connector.cursor()
 
 form = cgi.FieldStorage()
 
-hen = form.getvalue('foo')
+comm = form.getvalue('function')
+names = form.getvalue('name')
+s_stocks = form.getvalue('amount')
+s_kakaku = form.getvalue('price')
 
-if ' ' in hen or '-' in hen or '*' in hen or '/' in hen:
-    if ' ' in hen:
-        newhen = hen.replace(' ', '+')
-        print "Content-type: text/html\n"
-        print eval(newhen)
-    else:
-        print "Content-type: text/html\n"
-        print eval(hen)
-else:
-    print "Content-type: text/html\n"
-    print "ERROR"
+stocks = None
+kakaku = None
+
+if comm != None:
+    if comm == 'deleteall'
+        sql = "drop table product"
+        cur.execute(sql)
+        sql = "create table product(shurui char(10), namae char(50), stock integer)"
+        cur.execute(sql)
+
+if s_stocks != None:
+    if (isinstance(eval(s_stocks),(int, long))):
+        stocks = eval(s_stocks)
+    else:
+        print "Content-type: text/html\n"
+        print "ERROR"
+if s_kakaku != None:
+    if (isinstance(eval(s_kakaku),(int, long))):
+       kakaku = eval(s_kakaku)
+    else:
+        print "Content-type: text/html\n"
+        print "ERROR"
+
+connector.commit()
+
+
+cur.close()
+connector.close()
+
 
 html_body="""
 <html><body>
 %s
 </body></html>"""
-~                        
+
+        
